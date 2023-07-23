591388a73824c39cb0333e72c675948f9f37090a
jhon <fr.jhonk@gmail.com>
2023-07-23 00:35:02 -0500

    details

--- src/pages/Productos.jsx
+++ src/pages/Productos.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
 
-const data_lana = [
+export const data_lana = [
   {
     id: 1,
-    tipo: "PNA",
+    tipo: "lana",
+    name_ref: "APN",
     img: [
       "https://www.mundolanar.com/blog/wp-content/uploads/2013/12/tres-conos.jpg",
       "https://pe.all.biz/img/pe/catalog/24521.jpeg",
@@ -13,7 +14,8 @@ const data_lana = [
   },
   {
     id: 2,
-    tipo: "Pluma",
+    tipo: "lana",
+    name_ref: "Pluma",
     img: [
       "https://www.mundolanar.com/blog/wp-content/uploads/2013/12/tres-conos.jpg",
       "https://pe.all.biz/img/pe/catalog/24521.jpeg",
@@ -23,13 +25,12 @@ const data_lana = [
   },
   {
     id: 3,
-    tipo: "Pluma brillante",
+    tipo: "lana",
+    name_ref: "Pluma de cristal",
     img: [
       "https://www.mundolanar.com/blog/wp-content/uploads/2013/12/tres-conos.jpg",
       "https://pe.all.biz/img/pe/catalog/24521.jpeg",
     ],
-    price_sol: 36,
-    price_dol: 4,
   },
 ];
 
@@ -43,44 +44,48 @@ import yarn_icon from "../assets/icons/yarn.svg";
 export default function Productos() {
   // const [data, setData] = useState([]);
   // setData(data_lana);
-  const proElements = data_lana.map((lana) => (
-    <div key={lana.id} className="product-container">
-      <h3 className="product-name">{lana.tipo}</h3>
-      <div className="product-presentation">
-        <img src={lana.img[0]} className="product-main-img" />
-        <div className="product-img-info">
-          <div className="product-images">
-            <img src={lana.img[0]} />
-            <img src={lana.img[1]} />
-            <img src={lana.img[0]} />
-            <img src={lana.img[1]} />
-            <img src={lana.img[0]} />
-            <img src={lana.img[1]} />
-          </div>
-          <button>
-            <Link to={`/products/${lana.id}`} className="button-info">
-              <span>Mas informacion</span>
-              <svg viewBox="0 0 19 10" fill="none">
-                <path
-                  d="M18.3536 4.85355C18.5488 4.65829 18.5488 4.34171 18.3536 4.14645L15.1716 0.964466C14.9763 0.769204 14.6597 0.769204 14.4645 0.964466C14.2692 1.15973 14.2692 1.47631 14.4645 1.67157L17.2929 4.5L14.4645 7.32843C14.2692 7.52369 14.2692 7.84027 14.4645 8.03553C14.6597 8.2308 14.9763 8.2308 15.1716 8.03553L18.3536 4.85355ZM0 5H18V4H0V5Z"
-                  // fill="red"
+  // const imgs_show
+
+  const proElements = data_lana.map((lana, index) => {
+    const [actualImg, setActualImg] = useState(0);
+    return (
+      <div key={lana.id} className="product-container">
+        <h3 className="product-name">{lana.name_ref}</h3>
+        <div className="product-presentation">
+          {/* {const [actualImg, setActualImg] = useState(0)} */}
+          <img src={lana.img[actualImg]} className="product-main-img" />
+          <div className="product-img-info">
+            <div className="product-images">
+              {lana.img.map((img, index) => (
+                <img
+                  key={index}
+                  src={img}
+                  onClick={() => setActualImg(index)}
                 />
-              </svg>
-            </Link>
-          </button>
+              ))}
+            </div>
+            <button>
+              <Link to={`/products/${lana.id}`} className="button-info">
+                <span>Mas informacion</span>
+                <svg viewBox="0 0 19 10" fill="none">
+                  <path
+                    d="M18.3536 4.85355C18.5488 4.65829 18.5488 4.34171 18.3536 4.14645L15.1716 0.964466C14.9763 0.769204 14.6597 0.769204 14.4645 0.964466C14.2692 1.15973 14.2692 1.47631 14.4645 1.67157L17.2929 4.5L14.4645 7.32843C14.2692 7.52369 14.2692 7.84027 14.4645 8.03553C14.6597 8.2308 14.9763 8.2308 15.1716 8.03553L18.3536 4.85355ZM0 5H18V4H0V5Z"
+                    // fill="red"
+                  />
+                </svg>
+              </Link>
+            </button>
+          </div>
         </div>
-        {/* <div className="lana-info"> */}
-        {/* <h3 className="lana-type">{lana.tipo}</h3> */}
-        {/* </div> */}
       </div>
-    </div>
-  ));
+    );
+  });
   // console.log(data);
   return (
     <section className="product-list-container">
       <div className="icon-title">
         <img src={yarn_icon} alt="yarn" />
-        <h2> Lanas</h2>
+        <h2 className="tipe">{data_lana[0].tipo + "s"}</h2>
       </div>
       <hr />
       <div className="product-list">{proElements}</div>
--- src/pages/product_detail.jsx
+++ src/pages/product_detail.jsx
@@ -1,28 +1,9 @@
 import { useState } from "react";
 import { Link, NavLink, Outlet, Route, useParams } from "react-router-dom";
 
-const data_lana = [
-  {
-    id: 1,
-    tipo: "bola",
-    img: [
-      "https://www.mundolanar.com/blog/wp-content/uploads/2013/12/tres-conos.jpg",
-      "https://pe.all.biz/img/pe/catalog/24521.jpeg",
-    ],
-    price_sol: 36,
-    price_dol: 4,
-  },
-  {
-    id: 2,
-    tipo: "cono",
-    img: [
-      "https://www.mundolanar.com/blog/wp-content/uploads/2013/12/tres-conos.jpg",
-      "https://pe.all.biz/img/pe/catalog/24521.jpeg",
-    ],
-    price_sol: 36,
-    price_dol: 4,
-  },
-];
+import { data_lana } from "./Productos";
+import yarn_icon from "../assets/icons/yarn.svg";
+import "../assets/product_details.scss";
 
 function findLanaID(id) {
   const list1 = data_lana.filter((a) => a.id == id)[0];
@@ -31,25 +12,23 @@ function findLanaID(id) {
   const recibirParametro = (val) => setIndex(val);
   const details = (
     <div className="lana-detail-container">
-      <div className="lana-detail">
-        <img src={list1.img[index]}></img>
-        <div className="lana-detail-info-text">
-          <h2>{list1.tipo}</h2>
-          <p className="product-precio">$ {list1.price_dol}</p>
-          <p>{list1.description}</p>
-        </div>
-      </div>
-      <nav className="nav-description">
-        <NavLink end to=".">
-          fotos
-        </NavLink>
-        <NavLink to="colors">colors</NavLink>
-        <NavLink to="details">details</NavLink>
-      </nav>
-      <Outlet
-        context={{ list1, recibirParametro }}
-        // recibirParametro={recibirParametro}
-      />
+      {/* <div className="lana-detail"> */}
+      <img src={list1.img[index]} className="details-main-img"></img>
+      {/* </div> */}
+      <section className="nav-details">
+        <nav className="nav-description">
+          <NavLink end to=".">
+            Fotos
+          </NavLink>
+          <NavLink to="colors">Colores</NavLink>
+          <NavLink to="details">Detalles</NavLink>
+          <NavLink to="details">Precio</NavLink>
+        </nav>
+        <Outlet
+          context={{ list1, recibirParametro }}
+          // recibirParametro={recibirParametro}
+        />
+      </section>
     </div>
   );
   return details;
@@ -57,14 +36,25 @@ function findLanaID(id) {
 
 export default function ProductDetail() {
   const params = useParams();
-  // console.log(params);
+  const list1 = data_lana.filter((data) => data.id == params.id)[0];
+  // console.log(list1);
 
   return (
-    <section className="product-detail">
-      <Link to=".." className="back-button">
-        &larr; atras
-      </Link>
-      {data_lana[0] ? findLanaID(params.id) : <div>cargando</div>}
+    <section className="product-description-container">
+      <div className="icon-title">
+        <img src={yarn_icon} alt="yarn" />
+        <h2 className="tipe">
+          {list1.tipo} <span>- {list1.name_ref}</span>
+        </h2>
+      </div>
+      <hr />
+      <section className="product-detail-container">
+        <Link to=".." className="back-button">
+          <span>&larr;</span>
+          Volver a la lista
+        </Link>
+        {data_lana[0] ? findLanaID(params.id) : <div>cargando</div>}
+      </section>
     </section>
   );
 }
